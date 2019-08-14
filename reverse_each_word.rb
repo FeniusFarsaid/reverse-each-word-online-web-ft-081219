cf2e2a4c8763828ae40d7e5598c9487e2a7daa3f
David Ingram <david@ingramaviation.com>
2019-08-14 21:58:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   array = sentence.split("")
   reversed_sentence = []
-  array.each do |element| reversed_sentence << element.reverse}
+  array.each do |element| reversed_sentence << element.reverse
+  end 
   return reversed_sentence.join("")
 end 
 
